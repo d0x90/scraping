7f8fcf8c305ffeed4d4c563d38cf33a853481a36
Diego Campos Aquino <caquinodiego@crece.uss.edu.pe>
2016-06-05 18:17:14 -0500

    text inside tags

--- python/web_scraping/3.findAll/main.py
+++ python/web_scraping/3.findAll/main.py
@@ -4,6 +4,13 @@ from bs4 import BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
 bsObj = BeautifulSoup(html,"html.parser")
 
+#findAll(tag, attributes, recursive, text, limit, keywords)
+#find(tag, attributes, recursive, text, keywords)
+
+# we can search text inside tags too 
+#nameList = bsObj.findAll(text="the prince")
+#print(len(nameList))
+
 nameList = bsObj.findAll("span",{"class","green"})
 for name in nameList:
 	print name.get_text()

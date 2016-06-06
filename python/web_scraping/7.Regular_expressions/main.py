411740bb934baf7278636a024d44c9033414e948
Diego Campos Aquino <caquinodiego@crece.uss.edu.pe>
2016-06-05 22:39:43 -0500

    Regular expressions

---
+++ python/web_scraping/7.Regular_expressions/main.py
@@ -0,0 +1,13 @@
+#Let s say you have a problem, and you decide
+#to solve it with regular expressions. Well, now you have two problems
+
+from urllib import urlopen
+from bs4 import BeautifulSoup
+import re
+
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bsObj = BeautifulSoup(html,"html.parser")
+
+images = bsObj.findAll("img",{"src":re.compile("\.\.\/img\/gifts/img.*\.jpg")})
+for image in images :
+	print image["src"]

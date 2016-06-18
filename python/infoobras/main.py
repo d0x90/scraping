9647df0d686141361eecc1b73c47b89cfbb25495
Diego Campos Aquino <caquinodiego@crece.uss.edu.pe>
2016-06-18 11:51:47 -0500

    upd

--- python/infoobras/main.py
+++ python/infoobras/main.py
@@ -6,16 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-headers = {
-    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Origin': 'http://apps.contraloria.gob.pe/ciudadano/wfm_obras_buscador.aspx',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.17 (KHTML, like Gecko)  Chrome/24.0.1312.57 Safari/537.17',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Referer': 'http://www.nitt.edu/prm/nitreg/ShowRes.aspx',
-    'Accept-Encoding': 'gzip,deflate,sdch',
-    'Accept-Language': 'en-US,en;q=0.8',
-    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3'
-}
+
 chromedriver = "C:/Users/Diego Campos/chromedriver_win32/chromedriver.exe"
 url = "http://apps.contraloria.gob.pe/ciudadano/wfm_obras_buscador.aspx"
 driver = webdriver.Chrome(chromedriver)

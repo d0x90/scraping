9e12ac5b9b810078ba0288d57d2934edd972120a
Diego Campos Aquino <caquinodiego@crece.uss.edu.pe>
2016-06-18 13:16:28 -0500

    captcha

--- python/infoobras/main.py
+++ python/infoobras/main.py
@@ -1,12 +1,17 @@
+import base64
 import urllib
+from StringIO import StringIO
+
 from bs4 import BeautifulSoup
 from mechanize import Browser
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
 from selenium import webdriver
-
+from PIL import Image
+import pytesseract
 chromedriver = "C:/Users/Diego Campos/chromedriver_win32/chromedriver.exe"
 url = "http://apps.contraloria.gob.pe/ciudadano/wfm_obras_buscador.aspx"
 driver = webdriver.Chrome(chromedriver)
@@ -22,5 +27,33 @@ driver.find_element_by_xpath("""//*[@id="ddlProvincia"]/option[2]""").click()
 WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH,"""//*[@id="ddlDistrito"]""")))
 driver.find_element_by_xpath("""//*[@id="ddlDistrito"]/option[2]""").click()
 #Resolver Captcha
+WebDriverWait(driver,5).until(EC.presence_of_element_located((By.ID,"imgCaptcha")))
+
+#driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+#driver.save_screenshot('imagencaptcha.png')
+image_element = driver.find_element_by_xpath("""//*[@id="form1"]/div[2]/div[8]""")
+action_chain = ActionChains(driver)
+action_chain.move_to_element(image_element)
+action_chain.perform()
+
+loc, size = image_element.location_once_scrolled_into_view, image_element.size
+left, top = loc['x'], loc['y']
+
+width, height = size['width'], size['height']
+box = (int(left), int(top), int(left + width), int(top + height))
+
+screenshot = driver.save_screenshot("captcha.png")
+img = Image.open('captcha.png')
+captcha = img.crop(box)
+captcha.save('captcha.png', 'PNG')
+
+#aplicando OCR a la imagen
+image = Image.open('captcha.png')
+r,g,b,a = image.split()			#removing the alpha channel
+image = Image.merge('RGB',(r,g,b))
+resuelto = pytesseract.image_to_string(image)
 
 #submit
+driver.find_element_by_xpath("""//*[@id="txtCodCaptcha"]""").send_keys(resuelto)
+
